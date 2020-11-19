5ef3c2d06d2c99377497104f1c9420246b69dfb7
Remi Salmi <remi.salmi@gmail.com>
2020-11-19 14:55:24 +0100

    chnage cors

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 var cors = require('cors')
 
+var corsOptions = {
+    origin: 'https://alert-covid.ovh',
+  }
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 //Port to use
 const port = process.env.PORT || 4000;
